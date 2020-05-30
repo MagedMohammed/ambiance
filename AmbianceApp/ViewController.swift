3584d195c32ee54b4d7473a0076a33d720ca93fb
Maged Mohammed <maged.mo.it@gmail.com>
2020-05-30 17:07:26 +0200

    "first commit"

---
+++ AmbianceApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AmbianceApp
+//
+//  Created by Maged Omar on 5/29/20.
+//  Copyright © 2020 Maged Omar. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
